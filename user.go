f7a7c75324e1b25b7f20e29fa251c151e1aaeab5
opensvn <justleefeng@gmail.com>
2022-07-06 00:30:23 +0800

    add functions to set master public key

--- user.go
+++ user.go
@@ -56,6 +56,38 @@ func (u *User) GetEncryptMasterPublicKey() *sm9.EncryptMasterPublicKey {
 	return &u.encryptPrivateKey.EncryptMasterPublicKey
 }
 
+func (u *User) SetEncryptMasterPublicKey(s string) error {
+	buf, err := hex.DecodeString(s)
+	if err != nil {
+		return err
+	}
+
+	key := new(sm9.EncryptMasterPublicKey)
+	err = key.UnmarshalASN1(buf)
+	if err != nil {
+		return err
+	}
+
+	u.encryptPrivateKey.EncryptMasterPublicKey = *key
+	return nil
+}
+
 func (u *User) GetSignMasterPublicKey() *sm9.SignMasterPublicKey {
 	return &u.signPrivateKey.SignMasterPublicKey
+}
+
+func (u *User) SetSignMasterPublicKey(s string) error {
+	buf, err := hex.DecodeString(s)
+	if err != nil {
+		return err
+	}
+
+	key := new(sm9.SignMasterPublicKey)
+	err = key.UnmarshalASN1(buf)
+	if err != nil {
+		return err
+	}
+
+	u.signPrivateKey.SignMasterPublicKey = *key
+	return nil
 }
