b5efc6a88a3cd3e21d416920985e5d8c9d6d221b
opensvn <justleefeng@gmail.com>
2022-07-05 23:51:45 +0800

    modify user struct, remove kgc

--- user.go
+++ user.go
@@ -3,96 +3,59 @@ package client
 import (
 	"encoding/hex"
 	"github.com/emmansun/gmsm/sm9"
-	"github.com/opensvn/kgc"
 )
-
-var Kgc *kgc.Kgc
-var CurrentUser *User
-
-func init() {
-	k, err := kgc.New()
-	if err != nil {
-		panic(err)
-	}
-	Kgc = k
-}
 
 type User struct {
 	Uid               []byte
 	Hid               byte
-	signPrivateKey    *sm9.SignPrivateKey
-	signPublicKey     *sm9.SignMasterPublicKey
 	encryptPrivateKey *sm9.EncryptPrivateKey
-	encryptPublicKey  *sm9.EncryptMasterPublicKey
+	signPrivateKey    *sm9.SignPrivateKey
 }
 
-func NewUser(kgc *kgc.Kgc, uid []byte, hid byte) *User {
-	signKey, err := kgc.GenerateUserSignKey(uid, hid)
-	if err != nil {
-		return nil
-	}
-
-	encryptKey, err := kgc.GenerateUserEncryptKey(uid, hid)
-	if err != nil {
-		return nil
-	}
-
-	return &User{
-		Uid:               uid,
-		Hid:               hid,
-		signPrivateKey:    signKey,
-		signPublicKey:     signKey.MasterPublic(),
-		encryptPrivateKey: encryptKey,
-		encryptPublicKey:  encryptKey.MasterPublic(),
-	}
+func (u *User) GetEncryptPrivateKey() *sm9.EncryptPrivateKey {
+	return u.encryptPrivateKey
 }
 
-func (u *User) GetSignMasterPublicKeyASN1() string {
-	buf, err := u.signPublicKey.MarshalASN1()
-	if err != nil {
-		return ""
-	}
-
-	return hex.EncodeToString(buf)
-}
-
-func (u *User) SetSignMasterPublicKeyASN1(s string) error {
+func (u *User) SetEncryptPrivateKey(s string) error {
 	buf, err := hex.DecodeString(s)
 	if err != nil {
 		return err
 	}
 
-	masterKey := new(sm9.SignMasterPublicKey)
-	err = masterKey.UnmarshalASN1(buf)
+	key := new(sm9.EncryptPrivateKey)
+	err = key.UnmarshalASN1(buf)
 	if err != nil {
 		return err
 	}
 
-	u.signPublicKey = masterKey
+	u.encryptPrivateKey = key
 	return nil
 }
 
-func (u *User) GetEncryptMasterPublicKeyASN1() string {
-	buf, err := u.encryptPublicKey.MarshalASN1()
-	if err != nil {
-		return ""
-	}
-
-	return hex.EncodeToString(buf)
+func (u *User) GetSignPrivateKey() *sm9.SignPrivateKey {
+	return u.signPrivateKey
 }
 
-func (u *User) SetEncryptMasterPublicKeyASN1(s string) error {
+func (u *User) SetSignPrivateKey(s string) error {
 	buf, err := hex.DecodeString(s)
 	if err != nil {
 		return err
 	}
 
-	masterKey := new(sm9.EncryptMasterPublicKey)
-	err = masterKey.UnmarshalASN1(buf)
+	key := new(sm9.SignPrivateKey)
+	err = key.UnmarshalASN1(buf)
 	if err != nil {
 		return err
 	}
 
-	u.encryptPublicKey = masterKey
+	u.signPrivateKey = key
 	return nil
+}
+
+func (u *User) GetEncryptMasterPublicKey() *sm9.EncryptMasterPublicKey {
+	return &u.encryptPrivateKey.EncryptMasterPublicKey
+}
+
+func (u *User) GetSignMasterPublicKey() *sm9.SignMasterPublicKey {
+	return &u.signPrivateKey.SignMasterPublicKey
 }
