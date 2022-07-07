b697434631fbf59d5ccde81ed8f90eef26171f8e
opensvn <justleefeng@gmail.com>
2022-07-07 19:54:48 +0800

    fix corrupted caused by nil *paho.Auth

--- sm9_auth.go
+++ sm9_auth.go
@@ -19,33 +19,40 @@ func NewSm9Auth(c *Client) *Sm9Auth {
 }
 
 func (s *Sm9Auth) Authenticate(a *paho.Auth) *paho.Auth {
+	reauth := &paho.Auth{
+		Properties: &paho.AuthProperties{
+			AuthMethod: "sm9",
+		},
+		ReasonCode: packets.AuthReauthenticate,
+	}
+
 	s.Server = &User{}
 	s.Server.Uid = []byte(a.Properties.User.Get("uid"))
 	buf, err := hex.DecodeString(a.Properties.User.Get("hid"))
 	if err != nil {
-		return nil
+		return reauth
 	}
 	s.Server.Hid = buf[0]
 
 	buf, err = hex.DecodeString(string(a.Properties.AuthData))
 	if err != nil {
-		return nil
+		return reauth
 	}
 
 	decrypted, err := sm9.DecryptASN1(s.client.User.encryptPrivateKey, s.client.User.Uid, buf)
 	if err != nil {
-		return nil
+		return reauth
 	}
 
 	random1 := decrypted[:len(decrypted)/2]
 	if s.Random1 != hex.EncodeToString(random1) {
-		return nil
+		return reauth
 	}
 
 	random2 := decrypted[len(decrypted)/2:]
 	buf, err = sm9.EncryptASN1(rand.Reader, s.client.User.GetEncryptMasterPublicKey(), s.Server.Uid, s.Server.Hid, random2)
 	if err != nil {
-		return nil
+		return reauth
 	}
 
 	return &paho.Auth{
